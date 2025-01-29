85e7577386d65d9353e2f70969f8e1276a9b4559
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-01-29 17:04:51 +0100

    Finished writing the models of the diferent tables on the DB.

---
+++ Models/Acount.cs
@@ -0,0 +1,40 @@
+﻿namespace ProyectXAPI.Models
+{
+    public class Acount
+    {
+        const int MaxUsernameLength = 50;
+        const int MaxPasswordLength = 100;
+        private string _username;
+        private string _password;
+        public string Username 
+        {
+            get 
+            {
+                return _username;
+            }
+            set 
+            {
+                if(value.Length > MaxUsernameLength)
+                {
+                    throw new Exception("Username is too long");
+                }
+                _username = value; 
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if(value.Length > MaxPasswordLength)
+                {
+                    throw new Exception("Password is too long");
+                }
+                _password = value;
+            }
+        }
+    }
+}
---
+++ Models/SessionData.cs
@@ -0,0 +1,59 @@
+﻿namespace ProyectXAPI.Models
+{
+    public class SessionData
+    {
+        const int ProfileLength = 50;
+        const int ProfileCreatorLength = 50;
+        const int SessionLength = 10;
+
+        private Profile _profile;
+        private Session _session;
+        private int _kills;
+        private int _deaths;
+
+        public Profile Profile
+        {
+            get
+            {
+                return _profile;
+            }
+            set
+            {
+                _profile = value;
+            }
+        }
+        public Session Session
+        {
+            get
+            {
+                return _session;
+            }
+            set
+            {
+                _session = value;
+            }
+        }
+        public int Kills
+        {
+            get
+            {
+                return _kills;
+            }
+            set
+            {
+                _kills = value;
+            }
+        }
+        public int Deaths
+        {
+            get
+            {
+                return _deaths;
+            }
+            set
+            {
+                _deaths = value;
+            }
+        }
+    }
+}
